93016071e782cf646a2237f6d3322aad485cf1d4
Luca Farey <luca.farey@gmail.com>
2017-12-06 11:25:48 +0100

    Added basic module to do HTTP requests.

--- Android_Studio/app/src/main/java/com/huaweichallenge/app/MainActivity.java
+++ Android_Studio/app/src/main/java/com/huaweichallenge/app/MainActivity.java
@@ -4,6 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,4 +25,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_maps);
         startActivity(new Intent(MainActivity.this, MapsActivity.class));
     }
+
+    public void onHTTPButtonClick(View view) {
+
+        final TextView mTextView = (TextView) findViewById(R.id.textView2);
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://www.google.com";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        mTextView.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText("That didn't work!");
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
 }

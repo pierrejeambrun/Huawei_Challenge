1085415ec5198b7df69b0591317a302d30825b27
Pierre Jeambrun <pierre606@hotmail.fr>
2017-12-07 14:58:22 +0100

    View ok for logging

---
+++ Android_Studio/app/src/main/java/com/huaweichallenge/app/LoginActivity.java
@@ -0,0 +1,36 @@
+package com.huaweichallenge.app;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void loggingButtonClicked(View view) {
+        String username = ((EditText) findViewById(R.id.usernameField)).getText().toString();
+        String password = ((EditText) findViewById(R.id.passwordField)).getText().toString();
+        Log.w("Loggin Clicked", String.format("%s %s", username, password));
+    }
+}

de4951af0b3a2650552fb7415bc235e299a31642
Romain Marquant <romain.marquant12@gmail.com>
2017-12-09 00:37:46 +0100

    More websocket logic

--- Python/main.py
+++ Python/main.py
@@ -2,20 +2,29 @@
 import tornado.ioloop as ioloop
 import tornado.web as web
 import tornado.websocket as websocket
+import json
 
 class webSocketHandler(websocket.WebSocketHandler):
     def open(self):
             print("WebSocket opened")
 
     def check_origin(self, origin):
-        return True
+        return True #fixes forbidden code
 
     def on_message(self, message):
-        print(message)
-        self.write_message(u"You said: " + message)
+        try:
+            data = json.loads(message)
+        except ValueError:
+            self.write_message(json.dumps({"success": False, "error": "invalid json"}))
+
+        self.write_message(json.dumps({"success": True, "data": self.handleData(data)}))
 
     def on_close(self):
         print("WebSocket closed")
+
+    def handleData(self, data):
+        #DO STUFF HERE
+        return data
 
 def make_app():
     return web.Application([

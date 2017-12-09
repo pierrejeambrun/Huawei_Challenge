7288b97d2c84b6a7c5afae580f4117e38c1fc8ad
Romain Marquant <you@example.com>
2017-12-09 09:33:20 +0000

    Format to Array

--- Python/main.py
+++ Python/main.py
@@ -3,6 +3,7 @@ import tornado.ioloop as ioloop
 import tornado.web as web
 import tornado.websocket as websocket
 import tornado.httpserver as httpserver
+from random import randint
 import json
 
 class MainHandler(web.RequestHandler):
@@ -18,8 +19,30 @@ class webSocketHandler(websocket.WebSocketHandler):
 
     def on_message(self, message):
         try:
-            data = json.loads(message)
-        except ValueError:
+            parsedData = json.loads(message)
+
+            #["a_magnitude", "a_frequency", "a_mean", "a_sddeviation", "g_magnitude", "g_frequency", "g_mean", "g_sddeviation"]
+            # {
+            #     u'accelerationMagnitudeMean': 0.13395412,
+            #     u'gyroscopicFrequency': 0,
+            #     u'gyroscopicMagnitudeMean': 0.002657922,
+            #     u'accelerationMean': -0.13637818,
+            #     u'gyroscopicStd': 0.01168305,
+            #     u'accelerationFrequency': 0,
+            #     u'gyroscopicMean': -0.0014103942,
+            #     u'accelerationStd': 0.017011141
+            # }
+            data = [
+                parsedData['accelerationMagnitudeMean'],
+                parsedData['accelerationFrequency'],
+                parsedData['accelerationMean'],
+                parsedData['accelerationStd'],
+                parsedData['gyroscopicMagnitudeMean'],
+                parsedData['gyroscopicFrequency'],
+                parsedData['gyroscopicMean'],
+                parsedData['gyroscopicStd']
+            ]
+	except ValueError:
             self.write_message(json.dumps({"success": False, "error": "invalid json"}))
 
         self.write_message(json.dumps({"success": True, "data": self.handleData(data)}))
@@ -29,7 +52,8 @@ class webSocketHandler(websocket.WebSocketHandler):
 
     def handleData(self, data):
         #DO STUFF HERE
-        return data
+	print(data)
+        return randint(1,4)
 
 def make_app():
     return web.Application([
